9ebc88b805928e6a89547b6ca6d898517333e7ac
Bob <negenentwintig@hotmail.com>
2020-12-06 14:17:47 +0100

    better error messages

--- lib/wwwsand_web/controllers/page_controller.ex
+++ lib/wwwsand_web/controllers/page_controller.ex
@@ -11,8 +11,14 @@ defmodule WwwsandWeb.PageController do
         case Sand.run(%Sand{max_reductions: 10_000_000}, code) do
           {:ok, res, _box} ->
             json(conn, %{ok: true, result: inspect res})
+          {:error, :killed} ->
+            json(conn, %{ok: false, error: "Your program used too much memory."})
+          {:error, :max_reductions} ->
+            json(conn, %{ok: false, error: "Your program is too computationally intensive."})
+          {:error, {e, stack}} ->
+            json(conn, %{ok: false, error: Exception.format(:error, e, stack)})
           {:error, e} ->
-            json(conn, %{ok: false, error: inspect e})
+            json(conn, %{ok: false, error: Exception.format(:error, e)})
         end
       {:error, i} ->
         json(conn, %{ok: false, error: """
